5857b6f2021a85af605bd83eebf6091abfd094e0
Stashmaniac <stasa.premec@skole.hr>
2016-03-08 17:33:42 +0100

    ddaadlllkurats

--- index.php
+++ index.php
@@ -1,33 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">prog.local
-    <title>Naslov</title>
+    <meta charset="UTF-8">
+    <title>Forme</title>
 </head>
 
 <body>
-<h1>Prvi</h1>
 
-<p>
-    <?php
-    echo('Pozdrav iz PHP-a');
-    echo("<br>");
-    echo(date("d.m.y"));
-
-    $date1 = date('Y-m-d');
-    $date2 = date('2016-03-18')
-    $diffrece = date_diff()
-    ?>
-
-</p>
-
-<p>
-    <?php
-    $asspoop = 69;
-    echo("Do praznika je ostalo " . "<b>" . $asspoop . "</b>" . " dana.");
-    ?>
-</p>
-
+<h1>Forme</h1>
 
 
 </body>
